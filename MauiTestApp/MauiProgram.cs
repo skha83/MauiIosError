0a537fff7e1ea56773cd49eeb629ed69e327a2a9
Søren Hansen <skha@uneeg.com>
2023-10-12 10:14:47 +0200

    Initial commit

---
+++ MauiTestApp/MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Compatibility.Hosting;
+
+namespace MauiTestApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCompatibility()
+                .ConfigureMauiHandlers(handlers =>
+                {
+#if __ANDROID__
+                    handlers.AddCompatibilityRenderer(typeof(cmbSDKMaui.ScannerControl), typeof(cmbSDKMaui.Android.ScannerControlRenderer));
+#endif
+#if __IOS__
+                    handlers.AddCompatibilityRenderer(typeof(cmbSDKMaui.ScannerControl), typeof(cmbSDKMaui.iOS.ScannerControlRenderer));
+#endif
+                })
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
